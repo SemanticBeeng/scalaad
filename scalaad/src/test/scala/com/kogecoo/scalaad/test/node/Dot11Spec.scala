31c8a5ab1f6af111618d2b879ea5e5960c357312
kogecoo <kogecoo@gmail.com>
2016-04-10 02:01:13 +0900

    test for dot

--- scalaad/src/test/scala/com/kogecoo/scalaad/test/node/Dot11Spec.scala
+++ scalaad/src/test/scala/com/kogecoo/scalaad/test/node/Dot11Spec.scala
@@ -15,7 +15,7 @@ object Dot11Spec extends Properties("Dotl11") with NodeSpecBase {
   def op(a: N1, b: N1): N0 = Dot11(a, b)
 
   def vec(a: N0, s: S1): N1 = VecFill(a, s)
-/*
+
   property("eval") = forAll(genN1_N1()) { case (a: N1, b: N1) =>
     op(a, b).eval[T0] shouldCloseTo (a.toStd dot b.toStd)
   }
@@ -39,7 +39,6 @@ object Dot11Spec extends Properties("Dotl11") with NodeSpecBase {
   property("nonvar1 dot nonvar1 reverse node0") = forAll(genNV1_NV1(), genN0()) { case ((a: N1, b: N1), c: N0) =>
     op(a, b).reverse(c).size == 0
   }
-*/
   property("var1 dot nonvar1 reverse node0") = forAll(genV1_NV1(), genN0()) { case ((a: Var1, b: N1), c: N0) =>
     val g = op(a, b).reverse(c)
     g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo const1(b.toStd.sum * c.toStd, a.shape)
@@ -47,39 +46,39 @@ object Dot11Spec extends Properties("Dotl11") with NodeSpecBase {
 
   property("var1 dot nonvar1 reverse node1") = forAll(genV1_NV1_N1()) { case(a: Var1, b: N1, c: N1) =>
     val g = op(a, b).reverse(c)
-    g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo (c.toStd mul b.toStd.sum)
+    g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo const1(c.toStd dot b.toStd, a.shape)
   }
 /*
   property("var1 dot nonvar1 reverse node2") = forAll(genV1_NV1_RowEquivN2()) { case (a: Var1, b: N1, c: N2) =>
     val g = op(a, b).reverse(c)
     g(a).get.asInstanceOf[N2].eval[T2] shouldCloseTo (c.toStd colMul b.toStd)
   }
-
+*/
   property("nonvar1 dot var1 reverse node0") = forAll(genNV1_V1(), genN0()) { case ((a: N1, b: Var1), c: N0) =>
     val g = op(a, b).reverse(c)
-    g(b).get.asInstanceOf[N1].eval[T1] shouldCloseTo (a.toStd mul c.toStd)
+    g(b).get.asInstanceOf[N1].eval[T1] shouldCloseTo const1(a.toStd.sum * c.toStd, a.shape)
   }
 
   property("nonvar1 dot var1 reverse node1") = forAll(genNV1_V1_N1()) { case (a: N1, b: Var1, c: N1) =>
     val g = op(a, b).reverse(c)
-    g(b).get.asInstanceOf[N1].eval[T1] shouldCloseTo (c.toStd mul a.toStd)
+    g(b).get.asInstanceOf[N1].eval[T1] shouldCloseTo const1(a.toStd dot c.toStd, a.shape)
   }
-
+/*
   property("nonvar1 dot var1 reverse node2") = forAll(genNV1_V1_RowEquivN2()) { case (a: N1, b: Var1, c: N2) =>
     val g = op(a, b).reverse(c)
     g(b).get.asInstanceOf[N2].eval[T2] shouldCloseTo (c.toStd colMul a.toStd)
   }
-
+*/
   property("var1 dot var1 reverse node0") = forAll(genV1(), genN0()) { (a: Var1, b: N0) =>
     val g = op(a, a).reverse(b)
-    g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo (a.toStd mul (2 * b.toStd))
+    g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo const1(a.toStd.sum * b.toStd * 2.0, a.shape)
   }
 
   property("var1 dot var1 reverse node1") = forAll(genV1_N1()) { case (a: Var1, b: N1) =>
     val g = op(a, a).reverse(b)
-    g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo (a.toStd mul (b.toStd mul 2))
+    g(a).get.asInstanceOf[N1].eval[T1] shouldCloseTo const1((a.toStd dot b.toStd) * 2.0, a.shape)
   }
-
+/*
   property("var1 dot var1 reverse node2") = forAll(genV1_RowEquivN2()) { case (a: Var1, b: N2) =>
     val g = op(a, a).reverse(b)
     g(a).get.asInstanceOf[N2].eval[T2] shouldCloseTo (b.toStd colMul (a.toStd mul 2))
