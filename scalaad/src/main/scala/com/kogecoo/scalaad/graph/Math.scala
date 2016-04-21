c877defbb3814dcab91074af6c668f3a1be3f3f5
kogecoo <kogecoo@gmail.com>
2016-04-22 02:50:49 +0900

    wip

--- scalaad/src/main/scala/com/kogecoo/scalaad/graph/Math.scala
+++ scalaad/src/main/scala/com/kogecoo/scalaad/graph/Math.scala
@@ -3,109 +3,38 @@ package com.kogecoo.scalaad.graph
 import com.kogecoo.scalaad.{Shape0, Shape1, Shape2}
 
 
-// Element-wise Sin
 case class Sin0(v: N0) extends Op0
-case class Sin1(v: N1) extends Op1
-case class Sin2(v: N2) extends Op2
 
-// Element-wise Cos
 case class Cos0(v: N0) extends Op0
-case class Cos1(v: N1) extends Op1
-case class Cos2(v: N2) extends Op2
 
-// Element-wise Tan
 case class Tan0(v: N0) extends Op0
-case class Tan1(v: N1) extends Op1
-case class Tan2(v: N2) extends Op2
 
-// Element-wise Asin
 case class Asin0(v: N0) extends Op0
-case class Asin1(v: N1) extends Op1
-case class Asin2(v: N2) extends Op2
 
-// Element-wise Acos
 case class Acos0(v: N0) extends Op0
-case class Acos1(v: N1) extends Op1
-case class Acos2(v: N2) extends Op2
 
-// Element-wise Atan
 case class Atan0(v: N0) extends Op0
-case class Atan1(v: N1) extends Op1
-case class Atan2(v: N2) extends Op2
 
-// Element-wise Sinh
 case class Sinh0(v: N0) extends Op0
-case class Sinh1(v: N1) extends Op1
-case class Sinh2(v: N2) extends Op2
 
-// Element-wise Cosh
 case class Cosh0(v: N0) extends Op0
-case class Cosh1(v: N1) extends Op1
-case class Cosh2(v: N2) extends Op2
 
-// Element-wise Tanh
 case class Tanh0(v: N0) extends Op0
-case class Tanh1(v: N1) extends Op1
-case class Tanh2(v: N2) extends Op2
 
-// Element-wise Ln
 case class Ln0(v: N0) extends Op0
-case class Ln1(v: N1) extends Op1
-case class Ln2(v: N2) extends Op2
 
-// Element-wise Exp
 case class Exp0(v: N0) extends Op0
-case class Exp1(v: N1) extends Op1
-case class Exp2(v: N2) extends Op2
 
-// Element-wise Sqrt
 case class Sqrt0(v: N0) extends Op0
-case class Sqrt1(v: N1) extends Op1
-case class Sqrt2(v: N2) extends Op2
 
-// Element-wise Pow
 case class Pow00(l: N0, r: N0) extends Op00
-case class Pow11(l: N1, r: N1) extends Op11
-case class Pow22(l: N2, r: N2) extends Op22
 
-// Broadcast Pow
-case class Pow01(l: N0, r: N1) extends Op01
-case class Pow10(l: N1, r: N0) extends Op10
-case class Pow02(l: N0, r: N2) extends Op02
-case class Pow20(l: N2, r: N0) extends Op20
-case class Pow21(l: N2, r: N1) extends Op21
-case class Pow12(l: N1, r: N2) extends Op12
-
-// Element-wise Abs
 case class Abs0(v: N0) extends Op0
-case class Abs1(v: N1) extends Op1
-case class Abs2(v: N2) extends Op2
 
-// Element-wise Max
 case class Max00(l: N0, r: N0) extends Op00
-case class Max11(l: N1, r: N1) extends Op11
-case class Max22(l: N2, r: N2) extends Op22
 
-// Broadcast Max
-case class Max01(l: N0, r: N1) extends Op01
-case class Max02(l: N0, r: N2) extends Op02
-case class Max10(l: N1, r: N0) extends Op10
-case class Max20(l: N2, r: N0) extends Op20
-case class Max12(l: N1, r: N2) extends Op12
-case class Max21(l: N2, r: N1) extends Op21
-
-// Element-wise Min
 case class Min00(l: N0, r: N0) extends Op00
-case class Min11(l: N1, r: N1) extends Op11
-case class Min22(l: N2, r: N2) extends Op22
 
-// Broadcast Min
-case class Min01(l: N0, r: N1) extends Op01
-case class Min02(l: N0, r: N2) extends Op02
-case class Min10(l: N1, r: N0) extends Op10
-case class Min20(l: N2, r: N0) extends Op20
-case class Min12(l: N1, r: N2) extends Op12
-case class Min21(l: N2, r: N1) extends Op21
 
 // Experimental
 
--- scalaad/src/main/scala/com/kogecoo/scalaad/graph/Op.scala
+++ scalaad/src/main/scala/com/kogecoo/scalaad/graph/Op.scala
@@ -2,74 +2,33 @@ package com.kogecoo.scalaad.graph
 
 import com.kogecoo.scalaad.Shape2
 
-case class Elementwise11(l: N1, r: N1) extends Op11
-case class Elementwise22(l: N2, r: N2) extends Op22
 
-case class Broadcast01(l: N0, r: N1) extends Op02
-case class Broadcast02(l: N0, r: N2) extends Op02
-case class Broadcast12(l: N1, r: N2) extends Op12
+case class Apply0(v: N0, op: Op0)
 
-// Element-wise Add
+case class Elementwise1(v: N1, op: Op0)
+case class Elementwise2(v: N2, op: Op0)
+
+case class Broadcast1(v: N1, op: Op0)
+case class Broadcast2(v: N1, op: Op0)
+
+case class Elementwise11(l: N1, r: N1, op: Op00) extends Op11
+case class Elementwise22(l: N2, r: N2, op: Op00) extends Op22
+
+case class Broadcast01(l: N0, r: N1, op: Op00) extends Op02
+case class Broadcast02(l: N0, r: N2, op: Op00) extends Op02
+
+case class Rowwise12(l: N1, r: N2, op: Op00) extends Op12
+case class Columnwise12(l: N1, r: N2, op: Op00) extends Op12
+
+
 case class Add00(l :N0, r: N0) extends Op00
-case class Add11(l :N1, r: N1) extends Op11
-case class Add22(l :N2, r: N2) extends Op22
-
-// Broadcast Add
-case class Add01(l :N0, r: N1) extends Op01
-case class Add10(l :N1, r: N0) extends Op10
-case class Add02(l :N0, r: N2) extends Op02
-case class Add20(l :N2, r: N0) extends Op20
-case class Add12(l :N1, r: N2) extends Op12
-case class Add21(l :N2, r: N1) extends Op21
-
-// Element-wise Sub
 case class Sub00(l :N0, r: N0) extends Op00
-case class Sub11(l :N1, r: N1) extends Op11
-case class Sub22(l :N2, r: N2) extends Op22
-
-// Broadcast Sub
-case class Sub01(l :N0, r: N1) extends Op01
-case class Sub10(l :N1, r: N0) extends Op10
-case class Sub02(l :N0, r: N2) extends Op02
-case class Sub20(l :N2, r: N0) extends Op20
-case class Sub12(l :N1, r: N2) extends Op12
-case class Sub21(l :N2, r: N1) extends Op21
-
-// Element-wise Mul
 case class Mul00(l :N0, r: N0) extends Op00
-case class Mul11(l :N1, r: N1) extends Op11
-case class Mul22(l :N2, r: N2) extends Op22
-
-// Broadcast Mul
-case class Mul01(l :N0, r: N1) extends Op01
-case class Mul10(l :N1, r: N0) extends Op10
-case class Mul02(l :N0, r: N2) extends Op02
-case class Mul20(l :N2, r: N0) extends Op20
-case class Mul12(l :N1, r: N2) extends Op12
-case class Mul21(l :N2, r: N1) extends Op21
-
-// Element-wise Div
 case class Div00(l :N0, r: N0) extends Op00
-case class Div11(l :N1, r: N1) extends Op11
-case class Div22(l :N2, r: N2) extends Op22
-
-// Broadcast Div
-case class Div01(l :N0, r: N1) extends Op01
-case class Div10(l :N1, r: N0) extends Op10
-case class Div02(l :N0, r: N2) extends Op02
-case class Div20(l :N2, r: N0) extends Op20
-case class Div12(l :N1, r: N2) extends Op12
-case class Div21(l :N2, r: N1) extends Op21
 
 // Element-wise Pos
 case class Pos0(v: N0) extends Op0
-case class Pos1(v: N1) extends Op1
-case class Pos2(v: N2) extends Op2
-
-// Element-wise Neg
 case class Neg0(v: N0) extends Op0
-case class Neg1(v: N1) extends Op1
-case class Neg2(v: N2) extends Op2
 
 // Transpose
 case class Transpose1(v: N1) extends Op1
